39c450038188f6b204894c2690789558f058c140
Kwickos <Kwickos@users.noreply.github.com>
2026-03-18 16:24:46 +0100

    Remove page transition animations that caused double-load flash

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -132,18 +132,7 @@ export default function Layout() {
 
       {/* Content */}
       <main className="flex-1 flex flex-col">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 12 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -12 }}
-            transition={{ duration: 0.2, ease: 'easeOut' }}
-            className="flex-1 flex flex-col"
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
+        <Outlet />
       </main>
     </div>
   )
